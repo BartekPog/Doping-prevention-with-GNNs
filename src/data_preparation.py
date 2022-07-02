99f42695f63be4475e83307cb3cc6d7e2516f066
Bartłomiej Pogodziński <bartek.pogod@gmail.com>
2022-07-02 13:51:12 +0200

    Add in-gender only swapping and ensure only single swap per sample

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 
 class DataPreprocessor:
-    def __init__(self, data_path='data/data.xlsx', swap_rate=0.01):
+    def __init__(self, data_path='data/data.xlsx', swap_rate=0.01, swap_within_gender=True):
         """
         :data_path: path to data file
-        :swap_rate: probability of swapping a given sample"""
+        :swap_rate: probability of swapping a given sample
+        :swap_within_gender: Shoud swapping happen only within gender"""
         self.data_path = data_path
         self.swap_rate = swap_rate
+        self.swap_within_gender = swap_within_gender
 
     def get_raw_dataframe(self, data_path=None):
         return pd.read_excel(data_path if data_path else self.data_path)
@@ -16,16 +18,14 @@ class DataPreprocessor:
     def get_dataframe(self):
         df = self.get_raw_dataframe()
         df = self._rename_columns(df.copy())
-        df = self._swap_samples(df.copy(), self.swap_rate)
         df = self._map_gender(df.copy())
         df = self._map_in_competition(df.copy())
+        df = self._swap_samples(df.copy(), self.swap_rate, self.swap_within_gender)
+        
         return df
 
     @staticmethod
     def _rename_columns(df):
-        """
-        Rename columns to match the data model.
-        """
         rename_dict = {
             colname: colname.lower().replace(' ', '_')
             for colname in df.columns
@@ -49,9 +49,6 @@ class DataPreprocessor:
     
     @staticmethod
     def _map_in_competition(df):
-        """
-        Map in competition column to boolean.
-        """
         df['in_competition'] = df['in_competition'].apply(lambda x: True if x=='Y' else False)
         return df
 
@@ -61,33 +58,80 @@ class DataPreprocessor:
             return index_mapping[row.name]
         return row.athlete_id_real
 
-    def _swap_samples(self, df, swap_rate=0.01):
-        """
-        Swap samples with given probability.
-        """
+    def _get_swap_mapping(self, df, swap_rate=0.01):
         df_reduced = df[df['total_observations'] > 3] # remove samples with less than 3 observations
 
         swap_rate = swap_rate if swap_rate else 0.01
+        swaps_number = int(len(df)*swap_rate/2)
 
-        indices_to_swap = random.sample(list(df_reduced.index), int(len(df)*swap_rate))
+        indices_to_swap_from = random.sample(list(df_reduced.index), swaps_number)
 
-        reordered_indices = [*indices_to_swap]
-        random.shuffle(reordered_indices)
+        indices_to_swap_to = random.sample(
+            list(df_reduced[~df_reduced.index.isin(indices_to_swap_from)].index), 
+            swaps_number
+        )
 
-        new_athletes = df.loc[reordered_indices, :].athlete_id_real
-
-        index_mapping = {
-            sample_index: swapped_athlete
-            for sample_index, swapped_athlete
-            in zip(indices_to_swap, new_athletes)
+        from_to = {
+            from_id: to_id
+            for from_id, to_id in 
+            zip(indices_to_swap_from, indices_to_swap_to)
         }
 
-        df['athlete_id'] = df.apply(self._map_athletes, axis=1, args=(index_mapping, ))
-        df['is_swapped'] = df['athlete_id'] != df['athlete_id_real']
-        
+        to_from = {
+            from_id: to_id
+            for from_id, to_id in 
+            zip(indices_to_swap_to, indices_to_swap_from)
+        }
 
+        swap_index_mapping = {**from_to, **to_from} # Make sure the mapping is two-directional
+
+        athlete_id_per_sample =  df.loc[swap_index_mapping.values(), "athlete_id_real"].copy()
+
+        swap_mapping = {
+            sample_id: {
+                'swap_sample_id': new_sample_id,
+                'new_athlete': athlete_id_per_sample[new_sample_id]
+            } for sample_id, new_sample_id 
+            in swap_index_mapping.items()
+        }
+
+        return swap_mapping
+    
+    @staticmethod
+    def _map_sample_swapping(row, swap_mapping):
+        if row['sample_id'] in swap_mapping.keys():
+            
+            mapping = swap_mapping[row.name]
+            row['swapped_with_sample_id'] = mapping['swap_sample_id']
+            row['athlete_id'] = mapping['new_athlete']
+        return row
+
+    def _add_sample_indexes(self, df):
+        """
+        Add sample indexes to dataframe.
+        """
+        df['sample_id'] = df.index
         return df
+
+    def _swap_samples(self, df, swap_rate=0.01, swap_within_gender=True):
+        """
+        Swap samples with given probability.
+        """
+        if swap_within_gender:
+            return pd.concat([
+                self._swap_samples(df[df['is_male'] == False].copy(), swap_rate=swap_rate, swap_within_gender=False),
+                self._swap_samples(df[df['is_male'] == True].copy(), swap_rate=swap_rate, swap_within_gender=False)
+            ])
+
+        swap_mapping = self._get_swap_mapping(df.copy(), swap_rate=swap_rate)
+
+        df['sample_id'] = df.index
+        df['athlete_id'] = df['athlete_id_real']
+
+        df_swapped = df.apply(self._map_sample_swapping, axis=1, args=(swap_mapping, ))
         
+        return df_swapped
+
     @staticmethod
     def _map_total_observations(row, observations_mapping):
         if row.athlete_id in observations_mapping.keys():
