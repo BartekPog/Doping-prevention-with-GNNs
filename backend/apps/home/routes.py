c8f772669550a024a8ed248a110d4fb2070f2b57
Akash Gautam <418akash@gmail.com>
2022-07-21 19:04:07 +0200

    Move flask submodule to backend directory

---
+++ backend/apps/home/routes.py
@@ -0,0 +1,67 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from apps.home import blueprint
+from flask import render_template, request
+from flask_login import login_required
+from jinja2 import TemplateNotFound
+
+
+@blueprint.route('/index')
+#@login_required
+def index():
+
+    return render_template('home/index.html', segment='index')
+
+
+@blueprint.route('/<template>')
+#@login_required
+def route_template(template):
+
+    try:
+
+        if not template.endswith('.html'):
+            template += '.html'
+
+        # Detect the current page
+        segment = get_segment(request)
+
+        # Serve the file (if exists) from app/templates/home/FILE.html
+        return render_template("home/" + template, segment=segment)
+
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+    except:
+        return render_template('home/page-500.html'), 500
+
+
+@blueprint.route('/verify_doping', methods  = ["POST", "GET"])
+def verify_doping():
+
+    if request.method == "POST":
+        hormone_1 = request.form['h1']
+        print("hormone_1", hormone_1)
+
+        # logic for processing for doping and modeling
+        generated_message = "Based on our analysis we conclude that the provided sample do not corrospond to any levels of doping."
+
+        return render_template('home/page-blank.html', generated_message = generated_message)
+
+
+# Helper - Extract current page name from request
+def get_segment(request):
+
+    try:
+
+        segment = request.path.split('/')[-1]
+
+        if segment == '':
+            segment = 'index'
+
+        return segment
+
+    except:
+        return None
